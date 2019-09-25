394295e86cd83259e5793e86bcb10a0d10435c87
Nguyen Bui <bui.minhnguyen96@gmail.com>
2019-09-25 07:32:55 +0300

    first commit

---
+++ app/src/main/java/com/example/assignment22/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.assignment22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button submitBtn, resetBtn;
+    EditText lastName, firstName, phoneNumber, comments;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lastName = (EditText)findViewById(R.id.lastName);
+        firstName = (EditText)findViewById(R.id.firstName);
+        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+        comments = (EditText) findViewById(R.id.comments);
+        submitBtn = (Button) findViewById(R.id.submitBtn);
+        resetBtn = (Button) findViewById(R.id.resetBtn);
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.onResetButtonClick();
+            }
+        });
+    }
+
+    protected void onResetButtonClick() {
+        lastName.setText("");
+        firstName.setText("");
+        phoneNumber.setText("");
+        comments.setText("");
+    }
+}
